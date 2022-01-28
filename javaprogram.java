20a3cf8436b11462f215e6e0ba9fcc9417a17250
Somagaini297 <93876306+Somagaini297@users.noreply.github.com>
2022-01-28 07:24:14 +0530

    Update javaprogram.java

--- javaprogram.java
+++ javaprogram.java
@@ -2,6 +2,6 @@ class javaprogram
 {
 public static void main(String[]args)
 {
-System.out.println("This is my first java program");
+System.out.println("This is my first java program on friday");
 }
 }
